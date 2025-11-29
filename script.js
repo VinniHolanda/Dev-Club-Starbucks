9ac1149ffa83b95fe88022c368091953e6d705bd
VinniHolanda <viniciussholanda@gmail.com>
2025-11-28 21:50:37 -0300

    clique no copo,muda o copo grande

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 let circulo = document.querySelector(".circulo");
+let imagemCopo = document.querySelector(".imagem-copo");
 
-function mudarCor(cor){   
-    
+function mudarCor(cor, numeroImagem) {
     circulo.style.backgroundColor = cor;
+    imagemCopo.src = "img/img" + numeroImagem + ".png";
 }
-
